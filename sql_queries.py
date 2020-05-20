0063fff04a2d432ae787c1deede6d0697d256d54
Tom11Butler <tombutler21@hotmail.co.uk>
2020-05-20 23:41:33 +0100

    Changed NOT NULL conditions on the address table. This is becuase around 0.4% of all addresses have a postcode that is not in the postcodes table, and so we cannot fulfill the rest of that information. Changed the tables to account for the new data from the area classifications. Changed the data taken by the addresses and houses_detail tables so that it only stores distinct rows and avoids unnecessary data redundancy.

--- sql_queries.py
+++ sql_queries.py
@@ -25,68 +25,48 @@ staging_house_sales_table_create = ("""
             , property_type varchar NOT NULL
             , new_build varchar NOT NULL
             , freehold varchar NOT NULL
-            , number varchar NOT NULL
-            , flat_number varchar NOT NULL
+            , paon varchar NOT NULL
+            , saon varchar NOT NULL
             , street varchar NOT NULL
-            , neighbourhood varchar NOT NULL
-            , city varchar NOT NULL
-            , district varchar NOT NULL
-            , region varchar NOT NULL
+            , locality varchar NOT NULL
+            , town_city varchar NOT NULL
+            , county varchar NOT NULL
+            , country varchar NOT NULL
             , record_status varchar NOT NULL
             , unknown varchar NOT NULL
         )
 """)
 
-# you might wonder why this is so large
-# it is because this is what the file is like, and post staging we pick out what we want
 staging_postcodes_table_create = ("""
     CREATE TABLE IF NOT EXISTS postcodes_staging
         (
             postcode1 varchar NOT NULL
             , postcode2 varchar NOT NULL
             , postcode3 varchar NOT NULL
-            , date_intro varchar NOT NULL
-            , user_type int NOT NULL 
-            , easting varchar NOT NULL
-            , northing varchar NOT NULL
-            , positional_quality int NOT NULL
-            , county_code varchar NOT NULL
-            , county_name varchar
-            , local_auth_code varchar
-            , local_auth_name varchar
-            , ward_code varchar
-            , ward_name varchar
-            , country_code varchar NOT NULL
-            , country_name varchar NOT NULL
-            , region_code varchar NOT NULL
-            , region_name varchar NOT NULL
-            , prlmntry_cnst_code varchar NOT NULL
-            , prlmntry_cnst_name varchar NOT NULL
-            , euro_elctrl_rgn_code varchar
-            , euro_elctrl_rgn_name varchar
-            , primary_care_trust_code varchar
-            , primary_care_trust_name varchar
-            , lower_super_output_area_code varchar
-            , lower_super_output_area_name varchar
-            , middle_super_output_area_code varchar
-            , middle_super_output_area_name varchar
-            , output_area_class_code varchar
-            , output_area_class_name varchar
+            , county_nm varchar NOT NULL
+            , local_auth_nm varchar NOT NULL
+            , ward_nm varchar NOT NULL
+            , country_nm varchar NOT NULL
+            , region_nm varchar
+            , parlmntry_cnstit_nm varchar NOT NULL
+            , euro_elctrl_rgn_nm varchar
+            , prmry_care_trst_nm varchar
+            , lower_super_output_area_nm varchar
+            , middle_super_output_area_nm varchar
             , longitude float NOT NULL
             , latitude float NOT NULL
-            , spatial_acc varchar
-            , last_upload varchar
+            , last_uploaded varchar NOT NULL
             , location varchar
-            , socrata_id varchar
-            , lower_layer_super_output_area varchar
-            , ward varchar
+            , area_class_1 varchar NOT NULL
+            , area_class_2 varchar NOT NULL
+            , area_class_3 varchar NOT NULL
         )
 """)
 
 house_sales_table_create = ("""
     CREATE TABLE IF NOT EXISTS house_sales
         (
-            sale_id int IDENTITY(0,1) NOT NULL
+            sale_id varchar NOT NULL
             , house_detail_id int NOT NULL
             , address_id int NOT NULL
             , price float NOT NULL
@@ -110,16 +90,20 @@ addresses_table_create = ("""
     CREATE TABLE IF NOT EXISTS addresses
         (
             address_id int IDENTITY(0,1) NOT NULL
-            , prefix varchar
-            , flat_num int
+            , paon varchar NOT NULL
+            , saon varchar
             , street varchar NOT NULL
-            , neighbourhood varchar NOT NULL
-            , town varchar NOT NULL
-            , city varchar NOT NULL
+            , locality varchar NOT NULL
+            , town_city varchar
+            , district varchar
+            , county varchar
+            , country varchar
             , postcode varchar NOT NULL
-            , longitude float NOT NULL
-            , latitude float NOT NULL
-            , area_classification varchar NOT NULL
+            , longitude float 
+            , latitude float 
+            , area_class_1 varchar 
+            , area_class_2 varchar 
+            , area_class_3 varchar
             , PRIMARY KEY (address_id)
         )
 """)
@@ -161,11 +145,11 @@ staging_postcodes_copy = ("""
 
 # FINAL TABLES
 
-# this one last because depends on IDs from others
 house_sales_table_insert = ("""
-    INSERT INTO house_sales(house_detail_id, address_id, price, transfer_date)
+    INSERT INTO house_sales(sale_id, house_detail_id, address_id, price, transfer_date)
     SELECT
-        1
+        sale_id
+        , 1
         , 1
         , price
         , cast(transfer_date as date)
@@ -175,7 +159,7 @@ house_sales_table_insert = ("""
 
 house_details_table_insert = ("""
     INSERT INTO houses_detail(property_type, new_build, freehold)
-    SELECT
+    SELECT DISTINCT
         property_type
         , new_build
         , freehold
@@ -184,25 +168,33 @@ house_details_table_insert = ("""
 """)
 
 address_table_insert = ("""
-    INSERT INTO addresses(prefix, flat_num, street, neighbourhood,
-                            town, city, postcode, longitude, latitude,
-                            area_classification)
+    INSERT INTO addresses(paon, saon, street, locality, town_city,
+                        county, country, postcode, longitude, latitude,
+                        area_class_1, area_class_2, area_class_3)
     SELECT
-        h.number
-        , 1
-        , h.street
-        , h.neighbourhood
-        , h.city
-        , h.city
-        , h.postcode
+        a.*
         , p.longitude
         , p.latitude
-        , p.output_area_class_name
+        , p.area_class_1
+        , p.area_class_2
+        , p.area_class_3
     FROM
-        house_sales_staging h
-        , postcodes_staging p
-    WHERE
-        h.postcode=p.postcode1
+        (
+            SELECT DISTINCT
+                paon
+                , saon
+                , street
+                , locality
+                , town_city
+                , county
+                , country
+                , postcode
+            FROM
+                house_sales_staging
+        ) AS a
+        LEFT JOIN postcodes_staging as p
+            ON
+                a.postcode IN (p.postcode1, p.postcode2, p.postcode3)
 """)
 
 date_table_insert = ("""
@@ -218,7 +210,20 @@ date_table_insert = ("""
         house_sales
 """)
 
+# Update queries for the ids in the house_sales table
 
+house_sales_house_detail_id_update = ("""
+    
+
+
+""")
+
+
+house_sales_address_id_update = ("""
+    
+
+
+""")
 
 # QUERY LISTS
 drop_table_queries = [staging_house_sales_table_drop,
@@ -242,3 +247,6 @@ insert_table_queries = [house_sales_table_insert,
                        house_details_table_insert,
                        address_table_insert,
                        date_table_insert]
+
+update_table_queries = [house_sales_house_detail_id_update,
+                        house_sales_address_id_update]
