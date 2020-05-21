e040069af6bf79b4243dabe9346fc2456c13994a
Tom11Butler <tombutler21@hotmail.co.uk>
2020-05-21 22:59:00 +0100

    Overhauled the process for the data pipeline. Now add features to the house_sales_staging table for the address and house details IDs. This is so we can easily update the staging table given it includes the information we want to match to so that we get the IDs right for each sale. After these updates, we then derive the house_sales and dates from the house_sales_staging table. Also include an address_staging table to help with not overloading the CPU when inserting the addresses into the final table. Essentially, we separate out the join into two separate queries.

--- sql_queries.py
+++ sql_queries.py
@@ -8,9 +8,10 @@ config.read('dwh.cfg')
 
 staging_house_sales_table_drop = "DROP TABLE IF EXISTS house_sales_staging"
 staging_postcodes_table_drop = "DROP TABLE IF EXISTS postcodes_staging"
+staging_addresses_table_drop = "DROP TABLE IF EXISTS address_staging"
 house_sales_table_drop = "DROP TABLE IF EXISTS house_sales"
-addresses_table_drop = "DROP TABLE IF EXISTS addresses"
-houses_table_drop = "DROP TABLE IF EXISTS houses_detail"
+addresses_table_drop = "DROP TABLE IF EXISTS address_detail_lkp"
+houses_table_drop = "DROP TABLE IF EXISTS house_detail_lkp"
 dates_table_drop = "DROP TABLE IF EXISTS dates"
 
 # CREATE TABLES
@@ -24,7 +25,7 @@ staging_house_sales_table_create = ("""
             , postcode varchar NOT NULL
             , property_type varchar NOT NULL
             , new_build varchar NOT NULL
-            , freehold varchar NOT NULL
+            , duration varchar NOT NULL
             , paon varchar NOT NULL
             , saon varchar NOT NULL
             , street varchar NOT NULL
@@ -32,8 +33,8 @@ staging_house_sales_table_create = ("""
             , town_city varchar NOT NULL
             , county varchar NOT NULL
             , country varchar NOT NULL
+            , transaction_type varchar NOT NULL
             , record_status varchar NOT NULL
-            , unknown varchar NOT NULL
         )
 """)
 
@@ -63,6 +64,21 @@ staging_postcodes_table_create = ("""
         )
 """)
 
+staging_addresses_table_create = ("""
+    CREATE TABLE IF NOT EXISTS addresses_staging
+        (
+            paon varchar NOT NULL
+            , saon varchar NOT NULL
+            , street varchar NOT NULL
+            , locality varchar NOT NULL
+            , town_city varchar NOT NULL
+            , county varchar NOT NULL
+            , country varchar NOT NULL
+            , postcode varchar NOT NULL
+        )
+
+""")
+
 house_sales_table_create = ("""
     CREATE TABLE IF NOT EXISTS house_sales
         (
@@ -71,23 +87,24 @@ house_sales_table_create = ("""
             , address_id int NOT NULL
             , price float NOT NULL
             , transfer_date date NOT NULL
+            , transaction_type varchar NOT NULL
             , PRIMARY KEY (sale_id)
         )
 """)
 
 house_table_create = ("""
-    CREATE TABLE IF NOT EXISTS houses_detail
+    CREATE TABLE IF NOT EXISTS house_detail_lkp
         (
             house_detail_id int IDENTITY(0,1) NOT NULL
             , property_type varchar NOT NULL
             , new_build varchar NOT NULL
-            , freehold varchar NOT NULL
+            , duration varchar NOT NULL
             , PRIMARY KEY (house_detail_ID)
         )
 """)
 
 addresses_table_create = ("""
-    CREATE TABLE IF NOT EXISTS addresses
+    CREATE TABLE IF NOT EXISTS address_detail_lkp
         (
             address_id int IDENTITY(0,1) NOT NULL
             , paon varchar NOT NULL
@@ -132,6 +149,18 @@ staging_house_sales_copy = ("""
 """).format(config.get('S3','HOUSE_SALES_DATA')
            , config.get('IAM_ROLE','ARN'))
 
+add_house_detail_id_col = ("""
+    ALTER TABLE house_sales_staging
+    ADD COLUMN house_detail_id int
+    DEFAULT 1
+""")
+
+add_address_id_col = ("""
+    ALTER TABLE house_sales_staging
+    ADD COLUMN address_id int
+    DEFAULT 1
+""")
+
 staging_postcodes_copy = ("""
     COPY postcodes_staging
     FROM {}
@@ -143,34 +172,53 @@ staging_postcodes_copy = ("""
            , config.get('IAM_ROLE','ARN'))
 
 
+
+staging_address_table_insert = ("""
+    INSERT INTO addresses_staging(paon, saon, street, locality, town_city,
+                                  county, country, postcode)
+        SELECT DISTINCT
+            paon
+            , saon
+            , street
+            , locality
+            , town_city
+            , county
+            , country
+            , postcode
+        FROM
+            house_sales_staging
+""")
+
 # FINAL TABLES
 
 house_sales_table_insert = ("""
-    INSERT INTO house_sales(sale_id, house_detail_id, address_id, price, transfer_date)
+    INSERT INTO house_sales(sale_id, house_detail_id, address_id, price,
+                            transaction_type, transfer_date)
     SELECT
         sale_id
-        , 1
-        , 1
+        , house_detail_id
+        , address_id
         , price
+        , transaction_type
         , cast(transfer_date as date)
     FROM
         house_sales_staging
 """)
 
 house_details_table_insert = ("""
-    INSERT INTO houses_detail(property_type, new_build, freehold)
+    INSERT INTO house_detail_lkp(property_type, new_build, duration)
     SELECT DISTINCT
         property_type
         , new_build
-        , freehold
+        , duration
     FROM
         house_sales_staging
 """)
 
 address_table_insert = ("""
-    INSERT INTO addresses(paon, saon, street, locality, town_city,
-                        county, country, postcode, longitude, latitude,
-                        area_class_1, area_class_2, area_class_3)
+    INSERT INTO address_detail_lkp(paon, saon, street, locality,town_city,
+                                   county, country, postcode, longitude, latitude,
+                                   area_class_1, area_class_2, area_class_3)
     SELECT
         a.*
         , p.longitude
@@ -178,23 +226,9 @@ address_table_insert = ("""
         , p.area_class_1
         , p.area_class_2
         , p.area_class_3
-    FROM
-        (
-            SELECT DISTINCT
-                paon
-                , saon
-                , street
-                , locality
-                , town_city
-                , county
-                , country
-                , postcode
-            FROM
-                house_sales_staging
-        ) AS a
+    FROM addresses_staging AS a
         LEFT JOIN postcodes_staging as p
-            ON
-                a.postcode IN (p.postcode1, p.postcode2, p.postcode3)
+            ON a.postcode = postcode3
 """)
 
 date_table_insert = ("""
@@ -213,21 +247,33 @@ date_table_insert = ("""
 # Update queries for the ids in the house_sales table
 
 house_sales_house_detail_id_update = ("""
-    
-
-
+    UPDATE house_sales_staging SET house_detail_id = a.house_detail_id
+    FROM house_detail_lkp a
+    WHERE
+        house_sales_staging.property_type = a.property_type AND
+        house_sales_staging.new_build = a.new_build AND
+        house_sales_staging.duration = a.duration
 """)
 
 
 house_sales_address_id_update = ("""
-    
-
-
+    UPDATE house_sales_staging SET address_id = a.address_id
+    FROM address_detail_lkp a
+    WHERE
+        house_sales_staging.paon = a.paon AND
+        house_sales_staging.saon = a.saon AND
+        house_sales_staging.street = a.street AND
+        house_sales_staging.locality = a.locality AND
+        house_sales_staging.town_city = a.town_city AND
+        house_sales_staging.county = a.county AND
+        house_sales_staging.country = a.country AND
+        house_sales_staging.postcode = a.postcode
 """)
 
 # QUERY LISTS
 drop_table_queries = [staging_house_sales_table_drop,
                      staging_postcodes_table_drop,
+                     staging_addresses_table_drop,
                      house_sales_table_drop,
                      addresses_table_drop,
                      houses_table_drop,
@@ -235,18 +281,21 @@ drop_table_queries = [staging_house_sales_table_drop,
 
 create_table_queries = [staging_house_sales_table_create,
                        staging_postcodes_table_create,
+                       staging_addresses_table_create,
                        house_sales_table_create,
                        house_table_create,
                        addresses_table_create,
                        dates_table_create]
 
 copy_table_queries = [staging_house_sales_copy,
-                     staging_postcodes_copy]
+                     staging_postcodes_copy,
+                     add_house_detail_id_col,
+                     add_address_id_col]
 
-insert_table_queries = [house_sales_table_insert,
-                       house_details_table_insert,
-                       address_table_insert,
-                       date_table_insert]
-
-update_table_queries = [house_sales_house_detail_id_update,
-                        house_sales_address_id_update]
+insert_table_queries = [staging_address_table_insert,
+                        house_details_table_insert,
+                        house_sales_house_detail_id_update,
+                        address_table_insert,
+                        house_sales_address_id_update,
+                        house_sales_table_insert,
+                        date_table_insert]
