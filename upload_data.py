a818414fa295e6b9fe296b5e78e0dd8f1c10055f
Tom11Butler <tombutler21@hotmail.co.uk>
2020-05-21 23:00:21 +0100

    Implemented a script for uploading the data to S3

---
+++ upload_data.py
@@ -0,0 +1,52 @@
+import logging
+import sys
+import configparser
+import boto3
+from botocore.exceptions import ClientError
+
+
+
+def upload_to_s3(filepath, destination):
+    '''
+    
+    Inputs:
+    - filepath: the filepath in your local directory
+    - destination: S3 we want to upload to
+    '''
+    
+    config = configparser.ConfigParser()
+    config.read('dwh.cfg')
+    
+    KEY                    = config.get('AWS','KEY')
+    SECRET                 = config.get('AWS','SECRET')
+    BUCKET                 = config.get('S3', 'BUCKET_NAME')
+    
+    s3 = boto3.resource('s3', region_name='eu-west-2',
+                    aws_access_key_id=KEY,
+                    aws_secret_access_key=SECRET)
+    
+    s3.meta.client.upload_file(Filename=filepath, Bucket=BUCKET, Key=destination)
+
+def main():
+    
+    
+    if len(sys.argv) == 5:
+
+        pp_filepath, pp_key, postcodes_filepath, postcodes_key = sys.argv[1:]
+        
+        print('Uploading postcodes ...')
+        upload_to_s3(postcodes_filepath, postcodes_key)
+        
+        print('Uploading prices paid data ...')
+        upload_to_s3(pp_filepath, pp_key)
+        
+        print('\nSuccessfully uploaded!\n')
+        
+    else:
+        print('In order, please provide: the filepath of the price paid data, '\
+              'its desired name in the S3 bucket, the filepath of the postcodes data, '\
+              'its desired name in the S3 bucket. \n\nExample: python upload_data.py '\
+              'pp-complete.csv prices_paid postcodes.csv postcodes')
+
+if __name__=="__main__":
+    main()
